bd35042090aab7b58916b59f1b3885d1bd5d9416
dave <downneck@gmail.com>
2023-01-31 13:39:28 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Terraform Usage.ai IAM module 
+
+This module creates the IAM components necessary to support Usage.ai's AWS cost optimization service.
+
+There are no user servicable parts within, thus no variables needed.
+
+## Usage
+
+````terraform
+module "usage_ai" {
+    source  = "blahblah"
+    version = "0.1.0"
+}
+````
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Requirements
+
+| Name | Version |
+|------|---------|
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 1.2.0 |
+| <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 4.40.0 |
+
+## Providers
+
+| Name | Version |
+|------|---------|
+| <a name="provider_aws"></a> [aws](#provider\_aws) | >= 4.40.0 |
+
+## Modules
+
+No modules.
+
+## Resources
+
+| Name | Type |
+|------|------|
+| [aws_iam_policy.usage_ai](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_policy) | resource |
+| [aws_iam_role.usage_ai](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role) | resource |
+| [aws_iam_role_policy_attachment.usage_ai](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/iam_role_policy_attachment) | resource |
+| [aws_caller_identity.current](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity) | data source |
+| [aws_iam_policy_document.usage_ai](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/iam_policy_document) | data source |
+
+## Inputs
+
+No inputs.
+
+## Outputs
+
+No outputs.
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
---
+++ main.tf
@@ -0,0 +1,76 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_iam_policy_document" "usage_ai" {
+  statement {
+    sid       = "1"
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "application-autoscaling:Describe*",
+      "autoscaling:Describe*",
+      "aws-portal:ViewBilling",
+      "aws-portal:ViewUsage",
+      "ce:Describe*",
+      "ce:Get*",
+      "ce:List*",
+      "ec2:Describe*",
+      "ec2:AcceptReservedInstancesExchangeQuote",
+      "ec2:CancelReservedInstancesListing",
+      "ec2:CreateReservedInstancesListing",
+      "ec2:DeleteQueuedReservedInstances",
+      "ec2:ModifyReservedInstances",
+      "ec2:PurchaseHostReservation",
+      "ec2:PurchaseReservedInstancesOffering",
+      "ecs:Describe*",
+      "ecs:List*",
+      "eks:Describe*",
+      "eks:List*",
+      "pricing:DescribeServices",
+      "pricing:GetAttributeValues",
+      "pricing:GetProducts",
+      "savingsplans:Describe*",
+      "savingsplans:List*",
+      "savingsplans:*",
+      "servicequotas:Get*",
+      "servicequotas:List*",
+      "sts:AssumeRole",
+      "support:*"
+    ]
+  }
+}
+
+resource "aws_iam_policy" "usage_ai" {
+  name        = "usage_ai"
+  description = "Provides access for Usage.ai to make RI purchases based on usage patterns"
+  policy      = data.aws_iam_policy_document.usage_ai.json
+}
+
+resource "aws_iam_role" "usage_ai" {
+  # Usage.ai's integration fails to work correctly if you don't call the role exactly "UsageAI".
+  name        = "UsageAI"
+  description = "Provides access for Usage.ai to make RI purchases based on usage patterns"
+
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Action" : "sts:AssumeRole",
+        "Principal" : {
+          "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        "Effect" : "Allow",
+        "Condition" : {
+          "StringEquals" : {
+            "sts:ExternalId" : "gpLmBRB546"
+          }
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "usage_ai" {
+  role       = aws_iam_role.usage_ai.name
+  policy_arn = aws_iam_policy.usage_ai.arn
+}
